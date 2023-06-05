60651a7d045c3604aad7e75e751d8496c81a9d6c
SimonGiebenhain <simon.giebenhain@uni-konstanz.de>
2023-06-05 18:51:44 +0200

    added comments to example_usage.py

--- scripts/dataset/example_usage.py
+++ scripts/dataset/example_usage.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     ####################################################################################################################
 
     all_subjects = manager.get_all_subjects()
-    print(f'The dataset contains subjects {all_subjects}')
+    print(f'The dataset contains subjects with IDs {all_subjects}')
 
     for subject in all_subjects:
 
@@ -37,6 +37,9 @@ if __name__ == '__main__':
             m_registration = manager.get_registration_mesh(subject=subject, expression=expression)
             m_scan = manager.get_raw_mesh(subject=subject, expression=expression)
 
+            print('Left: FLAME fitting, Middle: Registration (upsampled FLAME, refined with ARAP), Right: Raw Scan (slightly downsampled).')
+            if i % 2 != 0:
+                print('Here we show the texture that comes alongside the 3D scans.')
             pl = pv.Plotter(shape=(1, 3))
 
             pl.subplot(0, 0)
@@ -61,6 +64,7 @@ if __name__ == '__main__':
             pl.camera.focal_point = (0, 0.15, 0)
             pl.show()
 
+
     ####################################################################################################################
     print_segment('Facial Landmarks and Facial Anchor Points')
     ####################################################################################################################
@@ -72,6 +76,11 @@ if __name__ == '__main__':
     facial_anchors = manager.get_facial_anchors(subject=subject, expression=expression)
     m_scan = manager.get_raw_mesh(subject=subject, expression=expression)
     m_registration = manager.get_registration_mesh(subject=subject, expression=expression)
+
+    print('Left Registered Mesh, Right Raw Scan. Bother overlayed with:')
+    print('Facial Landmarks in white and')
+    print('the facial anchors used in NPHM in red')
+
     pl = pv.Plotter(shape=(1, 2))
 
     pl.subplot(0, 0)
@@ -96,11 +105,19 @@ if __name__ == '__main__':
     ####################################################################################################################
     print_segment('Planar Segmentation of torso')
     ####################################################################################################################
+    '''
+    For simplicity we separated the torso from the head using a plane defined by 3 vertices on the FLAME fitting.
+    For many people this removes a significant part of the hair.
+    In the future more elaborate rays to perform this separation are necessary.
+    '''
+
 
     head_mask = manager.cut_throat(m_scan.vertices, subject=subject, expression=expression)
 
     m_head = cut_trimesh_vertex_mask(m_scan.copy(), mask=head_mask)
 
+
+    print('Red area is used for training. The white area is discarded in preprocessing.')
     pl = pv.Plotter()
 
     pl.add_mesh(m_scan)
@@ -114,17 +131,21 @@ if __name__ == '__main__':
     print_segment('Single-View Observations')
     ####################################################################################################################
     '''
-    Note: This is an example of our evaluation of the models' performance.
-    
+    Note: This is an example of our evaluation of the models' performance. 
+    Time will tell whether the quality of observations is too high.
+    It might be usefull to further make the observation sparse, or add artificial noise, 
+    in order to make for a more challenging scenario.
     '''
 
+    print('Fitting is based on multiple single view observations. One for each expression.')
+    print('Additionally, we provide an observation from the back for the first expression only. This, in theory, enables more accurate reconstruction of hair.')
 
     for subject in all_subjects:
         expressions = manager.get_expressions(subject)
         pl = pv.Plotter(shape=(1, len(expressions)))
         for i, expression in enumerate(expressions):
 
-            obs = manager.get_single_view_obs(subject, expression)
+            obs = manager.get_single_view_obs(subject, expression, include_back=i ==0)
             pl.subplot(0, i)
             pl.add_points(obs)
 
